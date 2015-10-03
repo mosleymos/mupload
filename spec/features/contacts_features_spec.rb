6d52bf7568a71057711fa1ca5164d21aea0d0784
Mori <keitamori@gmail.com>
2015-10-03 17:24:11 +0200

    test: feature good contacts

--- spec/features/contacts_features_spec.rb
+++ spec/features/contacts_features_spec.rb
@@ -18,5 +18,18 @@ describe "Test expect send a csv file" do
 		click_button("Envoi")
 		expect(page).to have_text("Liste des contacts")
   end
-
 end
+
+describe "Test expect good list from csv file" do
+
+  feature "Test we should see a list of good contacts"
+  scenario "Should see a list of clean contacts" do
+    visit '/'
+    attach_file("contacts", "#{Rails.root + 'contacter.xlsx'}")
+		click_button("Envoi")
+		expect(page).to have_text("Paul")
+		expect(page).to have_text("Durant")
+		expect(page).to have_text("p.durant@gmail.com")
+  end
+end
+
