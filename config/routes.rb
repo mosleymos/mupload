05bde413f863cc7ed8f547c86979122c49effeb9
Mori <keitamori@gmail.com>
2015-10-03 12:15:23 +0200

    feat: upload a simple spreadsheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'contacts#index'
+	post 'upload' => 'contacts#upload'
+	get 'contacts/list' => 'contacts#list'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
