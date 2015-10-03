6aa25de4de5cb9c763247536a1616e507e307818
Mori <keitamori@gmail.com>
2015-10-03 19:32:24 +0200

    test: feature bad contacts

--- spec/features/contacts_features_spec.rb
+++ spec/features/contacts_features_spec.rb
@@ -9,7 +9,7 @@ describe "Test expect a homepage with about content" do
   end
 end
 
-describe "Test expect send a csv file" do
+describe "Test expect send a excel file" do
 
   feature "Test we should see a list of contacts"
   scenario "Should see a list of contacts" do
@@ -20,7 +20,7 @@ describe "Test expect send a csv file" do
   end
 end
 
-describe "Test expect good list from csv file" do
+describe "Test expect good list from excel file" do
 
   feature "Test we should see a list of good contacts"
   scenario "Should see a list of clean contacts" do
@@ -30,6 +30,23 @@ describe "Test expect good list from csv file" do
 		expect(page).to have_text("Paul")
 		expect(page).to have_text("Durant")
 		expect(page).to have_text("p.durant@gmail.com")
+  end
+end
+
+describe "Test expect to show unindentified contacts from excel file" do
+
+  feature "Test we should see a list of bad contacts too"
+  scenario "Should see a list of bad contacts" do
+    visit '/'
+    attach_file("contacts", "#{Rails.root + 'contacter.xlsx'}")
+		click_button("Envoi")
+		expect(page).to have_text("Paul")
+		expect(page).to have_text("Durant")
+		expect(page).to have_text("p.durant@gmail.com")
+		expect(page).to have_text("Contacts non identifiés")
+		expect(page).to have_text("Fl")
+		expect(page).to have_text("Bd")
+		expect(page).to have_text("n=martin@t§sd.fr")
   end
 end
 
