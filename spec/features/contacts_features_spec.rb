489a32f3bdd9e00ff033fd29f9fda4d16b902fc8
Mori <keitamori@gmail.com>
2015-10-03 12:13:14 +0200

    test: feature upload a spreadsheet

--- spec/features/contacts_features_spec.rb
+++ spec/features/contacts_features_spec.rb
@@ -5,5 +5,18 @@ describe "Test expect a homepage with about content" do
 	scenario "Should see a contact page" do
 	  visit '/'
 		expect(page).to have_text('Mupload csv')
+		expect(page).to have_button('Envoi')
   end
+end
+
+describe "Test expect send a csv file" do
+
+  feature "Test we should see a list of contacts"
+  scenario "Should see a list of contacts" do
+    visit '/'
+    attach_file("contacts", "#{Rails.root + 'contacter.xlsx'}")
+		click_button("Envoi")
+		expect(page).to have_text("Liste des contacts")
+  end
+
 end
