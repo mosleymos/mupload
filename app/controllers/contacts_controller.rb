2cabc4bd330964ffc5d18bc1af9eeccbcdc16b76
Mori <keitamori@gmail.com>
2015-10-06 18:06:43 +0200

    style: correction of code style

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,45 +1,33 @@
 class ContactsController < ApplicationController
-	class Contact
-	  attr_accessor :firstname, :lastname, :email, :status
-		def initialize(firstname, lastname, email)
-			@firstname= firstname
-			@lastname = lastname
-			@email = email
-			@status = false
-		end
+  class Contact
+    attr_accessor :firstname, :lastname, :email, :status
+    def initialize(firstname, lastname, email)
+      @firstname= firstname
+      @lastname = lastname
+      @email = email
+      @status = false
+    end
 
-		def email_valid?
-			regex_email=/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-		  (@email =~ regex_email) == 0
-		end
+    def email_valid?
+      regex_email=/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	    (@email =~ regex_email) == 0
+    end
 
-		def correct_contact
-		  regex = /[^a-zA-Z\d\s:]/
-			@firstname = @firstname.gsub(regex, '').capitalize
-			@lastname = @lastname.gsub(regex, '').capitalize
-			return self
-		end
+    def correct_contact
+      regex = /[^a-zA-Z\d\s:]/
+      @firstname = @firstname.gsub(regex, '').capitalize
+      @lastname = @lastname.gsub(regex, '').capitalize
+      return self
+    end
 		
-		def identity_valid?
-		  @firstname.length > 3 && @lastname.length > 3
-		end 
+    def identity_valid?
+      @firstname.length > 3 && @lastname.length > 3
+    end 
 
-		def contact_valid?
-			if identity_valid? && email_valid?
-        @status = true
-			else
-				@status = false
-			end
-		end
-
-		def to_a
-		  [@firstname,@lastname,@email]
-		end
-
-		def to_s
-		  "#{@firstname} #{@lastname} #{@email}"
-		end
-	end
+    def contact_valid?
+      identity_valid? && email_valid? ? @status = true : @status = false
+    end
+  end
 
   def index
   end
@@ -51,16 +39,16 @@ class ContactsController < ApplicationController
     xlsx = Roo::Spreadsheet.open(uploaded.original_filename)
     xlsx.each_with_pagename do |name, sheet|
       ((sheet.first_row+1).upto(sheet.last_row)).map do|r|
-         @import.push(Contact.new(xlsx.sheet(name).row(r)[0], xlsx.sheet(name).row(r)[1], xlsx.sheet(name).row(r)[2])
-)			end
+         @import.push(Contact.new(xlsx.sheet(name).row(r)[0], xlsx.sheet(name).row(r)[1], xlsx.sheet(name).row(r)[2]))
+      end
     end
 
-		@import = @import.uniq{|e| [e.firstname, e.lastname, e.email] }
-		@bad = @import.map{|e| e}.select{|f| f.correct_contact.contact_valid? == false }
-		@good = @import.map{|e| e}.select{|f| f.correct_contact.contact_valid? == true }
-
-		flash[:bad] = @bad
-		flash[:good] = @good 
+    @import = @import.uniq{|e| [e.firstname, e.lastname, e.email] }
+    @bad = @import.map{|e| e}.select{|f| f.correct_contact.contact_valid? == false }
+    @good = @import.map{|e| e}.select{|f| f.correct_contact.contact_valid? == true }
+    
+    flash[:bad] = @bad
+    flash[:good] = @good 
     redirect_to '/contacts/list'
 
   end
--- spec/features/contacts_features_spec.rb
+++ spec/features/contacts_features_spec.rb
@@ -1,69 +1,68 @@
 require 'rails_helper'
 
-describe "Test expect a homepage with about content" do
-  feature "Test we should see a contact page" 
+describe 'Test expect a homepage with about content' do
+  feature 'Test we should see a contact page' 
 	scenario "Should see a contact page" do
-	  visit '/'
+		visit '/'
 		expect(page).to have_text('Mupload csv')
 		expect(page).to have_button('Envoi')
   end
 end
 
-describe "Test expect send a excel file" do
+describe 'Test expect send a excel file' do
 
-  feature "Test we should see a list of contacts"
-  scenario "Should see a list of contacts" do
+  feature 'Test we should see a list of contacts'
+  scenario 'Should see a list of contacts' do
     visit '/'
-    attach_file("contacts", "#{Rails.root + 'contacter.xlsx'}")
-		click_button("Envoi")
-		expect(page).to have_text("Liste des contacts")
+    attach_file('contacts', "#{Rails.root + 'contacter.xlsx'}")
+		click_button('Envoi')
+		expect(page).to have_text('Liste des contacts')
   end
 end
 
-describe "Test expect good list from excel file" do
+describe 'Test expect good list from excel file' do
 
-  feature "Test we should see a list of good contacts"
-  scenario "Should see a list of clean contacts" do
+  feature 'Test we should see a list of good contacts'
+  scenario 'Should see a list of clean contacts' do
     visit '/'
-    attach_file("contacts", "#{Rails.root + 'contacter.xlsx'}")
-		click_button("Envoi")
-		expect(page).to have_text("Paul")
-		expect(page).to have_text("Durant")
-		expect(page).to have_text("p.durant@gmail.com")
+    attach_file('contacts', "#{Rails.root + 'contacter.xlsx'}")
+		click_button('Envoi')
+		expect(page).to have_text('Paul')
+		expect(page).to have_text('Durant')
+		expect(page).to have_text('p.durant@gmail.com')
   end
 end
 
-describe "Test expect a list with no repeated contacts" do
+describe 'Test expect a list with no repeated contacts' do
 
-  feature "Test we should see a list unique contacts"
-  scenario "Should see a list of unique contacts" do
+  feature 'Test we should see a list unique contacts'
+  scenario 'Should see a list of unique contacts' do
     visit '/'
-    attach_file("contacts", "#{Rails.root + 'contacter.xlsx'}")
-		click_button("Envoi")
-		expect(page).to have_text("Paul")
-		expect(page).to have_text("Durant")
-		expect(page).to have_text("p.durant@gmail.com")
-		expect(page).to have_text("Mori")
+    attach_file('contacts', "#{Rails.root + 'contacter.xlsx'}")
+		click_button('Envoi')
+		expect(page).to have_text('Paul')
+		expect(page).to have_text('Durant')
+		expect(page).to have_text('p.durant@gmail.com')
+		expect(page).to have_text('Mori')
 		expect(page).to have_text("Keita")
 
   end
 end
 
 
-describe "Test expect to show unindentified contacts from excel file" do
+describe 'Test expect to show unindentified contacts from excel file' do
 
-  feature "Test we should see a list of bad contacts too"
-  scenario "Should see a list of bad contacts" do
+  feature 'Test we should see a list of bad contacts too'
+  scenario 'Should see a list of bad contacts' do
     visit '/'
-    attach_file("contacts", "#{Rails.root + 'contacter.xlsx'}")
-		click_button("Envoi")
-		expect(page).to have_text("Paul")
-		expect(page).to have_text("Durant")
-		expect(page).to have_text("p.durant@gmail.com")
-		expect(page).to have_text("Contacts non identifiés")
-		expect(page).to have_text("Fl")
-		expect(page).to have_text("Bd")
-		expect(page).to have_text("n=martin@t§sd.fr")
+    attach_file('contacts', "#{Rails.root + 'contacter.xlsx'}")
+		click_button('Envoi')
+		expect(page).to have_text('Paul')
+		expect(page).to have_text('Durant')
+		expect(page).to have_text('p.durant@gmail.com')
+		expect(page).to have_text('Contacts non identifiés')
+		expect(page).to have_text('Fl')
+		expect(page).to have_text('Bd')
+		expect(page).to have_text('n=martin@t§sd.fr')
   end
 end
-
