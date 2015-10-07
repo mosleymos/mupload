05d3c310a14bedcd871736f58bf982a1c26fd0c4
Mori <keitamori@gmail.com>
2015-10-07 13:55:07 +0200

    style: tab correction of tests

--- spec/features/contacts_features_spec.rb
+++ spec/features/contacts_features_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe 'Test expect a homepage with about content' do
   feature 'Test we should see a contact page' 
-	scenario "Should see a contact page" do
-		visit '/'
-		expect(page).to have_text('Mupload csv')
-		expect(page).to have_button('Envoi')
+  scenario "Should see a contact page" do
+    visit '/'
+    expect(page).to have_text('Mupload csv')
+    expect(page).to have_button('Envoi')
   end
 end
 
@@ -15,8 +15,8 @@ describe 'Test expect send a excel file' do
   scenario 'Should see a list of contacts' do
     visit '/'
     attach_file('contacts', "#{Rails.root + 'contacter.xlsx'}")
-		click_button('Envoi')
-		expect(page).to have_text('Liste des contacts')
+    click_button('Envoi')
+    expect(page).to have_text('Liste des contacts')
   end
 end
 
@@ -26,10 +26,10 @@ describe 'Test expect good list from excel file' do
   scenario 'Should see a list of clean contacts' do
     visit '/'
     attach_file('contacts', "#{Rails.root + 'contacter.xlsx'}")
-		click_button('Envoi')
-		expect(page).to have_text('Paul')
-		expect(page).to have_text('Durant')
-		expect(page).to have_text('p.durant@gmail.com')
+    click_button('Envoi')
+    expect(page).to have_text('Paul')
+    expect(page).to have_text('Durant')
+    expect(page).to have_text('p.durant@gmail.com')
   end
 end
 
@@ -39,12 +39,12 @@ describe 'Test expect a list with no repeated contacts' do
   scenario 'Should see a list of unique contacts' do
     visit '/'
     attach_file('contacts', "#{Rails.root + 'contacter.xlsx'}")
-		click_button('Envoi')
-		expect(page).to have_text('Paul')
-		expect(page).to have_text('Durant')
-		expect(page).to have_text('p.durant@gmail.com')
-		expect(page).to have_text('Mori')
-		expect(page).to have_text("Keita")
+    click_button('Envoi')
+    expect(page).to have_text('Paul')
+    expect(page).to have_text('Durant')
+    expect(page).to have_text('p.durant@gmail.com')
+    expect(page).to have_text('Mori')
+    expect(page).to have_text("Keita")
 
   end
 end
@@ -56,13 +56,13 @@ describe 'Test expect to show unindentified contacts from excel file' do
   scenario 'Should see a list of bad contacts' do
     visit '/'
     attach_file('contacts', "#{Rails.root + 'contacter.xlsx'}")
-		click_button('Envoi')
-		expect(page).to have_text('Paul')
-		expect(page).to have_text('Durant')
-		expect(page).to have_text('p.durant@gmail.com')
-		expect(page).to have_text('Contacts non identifiés')
-		expect(page).to have_text('Fl')
-		expect(page).to have_text('Bd')
-		expect(page).to have_text('n=martin@t§sd.fr')
+    click_button('Envoi')
+    expect(page).to have_text('Paul')
+    expect(page).to have_text('Durant')
+    expect(page).to have_text('p.durant@gmail.com')
+    expect(page).to have_text('Contacts non identifiés')
+    expect(page).to have_text('Fl')
+    expect(page).to have_text('Bd')
+    expect(page).to have_text('n=martin@t§sd.fr')
   end
 end
