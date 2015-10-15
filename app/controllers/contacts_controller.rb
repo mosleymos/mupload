89a22a00d491a76020fbff72491f9bec7a5aa22c
Mori <keitamori@gmail.com>
2015-10-15 12:54:43 +0200

    refactor: contact in module Treat

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,46 +1,19 @@
 class ContactsController < ApplicationController
-  class Contact
-    attr_accessor :firstname, :lastname, :email, :status
-    def initialize(firstname, lastname, email)
-      @firstname= firstname
-      @lastname = lastname
-      @email = email
-      @status = false
-    end
-
-    def email_valid?
-      regex_email=/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-	    (@email =~ regex_email) == 0
-    end
-
-    def correct_contact
-      regex = /[^a-zA-Z\d\s:]/
-      @firstname = @firstname.gsub(regex, '').capitalize
-      @lastname = @lastname.gsub(regex, '').capitalize
-      return self
-    end
-		
-    def identity_valid?
-      @firstname.length > 3 && @lastname.length > 3
-    end 
-
-    def contact_valid?
-      identity_valid? && email_valid? ? @status = true : @status = false
-    end
-  end
-
+	
+	include Treat
+	
   def index
   end
 
   def upload
-
 		begin
+		create_contact = Treat::Contact
     uploaded = params[:contacts]
     @import, @good, @bad = [], [], []
     xlsx = Roo::Spreadsheet.open(uploaded.original_filename)
     xlsx.each_with_pagename do |name, sheet|
       ((sheet.first_row+1).upto(sheet.last_row)).map do|r|
-         @import.push(Contact.new(xlsx.sheet(name).row(r)[0], xlsx.sheet(name).row(r)[1], xlsx.sheet(name).row(r)[2]))
+         @import.push(create_contact.new(xlsx.sheet(name).row(r)[0], xlsx.sheet(name).row(r)[1], xlsx.sheet(name).row(r)[2]))
       end
     end
 
---
+++ lib/contacts/treat.rb
@@ -0,0 +1,32 @@
+module Treat 
+  class Contact
+    attr_accessor :firstname, :lastname, :email, :status
+    def initialize(firstname, lastname, email)
+      @firstname= firstname
+      @lastname = lastname
+      @email = email
+      @status = false
+    end
+
+    def email_valid?
+      regex_email=/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	    (@email =~ regex_email) == 0
+    end
+
+    def correct_contact
+      regex = /[^a-zA-Z\d\s:]/
+      @firstname = @firstname.gsub(regex, '').capitalize
+      @lastname = @lastname.gsub(regex, '').capitalize
+      return self
+    end
+		
+    def identity_valid?
+      @firstname.length > 3 && @lastname.length > 3
+    end 
+
+    def contact_valid?
+      identity_valid? && email_valid? ? @status = true : @status = false
+    end
+  end
+
+end
