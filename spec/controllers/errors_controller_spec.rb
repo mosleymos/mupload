29dfcfd8e04305b0c800b3b76169b3fe8a1df5b3
Mori <keitamori@gmail.com>
2015-10-23 18:30:30 +0200

    test: correct not_found add server_error

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 RSpec.describe ErrorsController, type: :controller do
 
   describe "GET #not_found" do
-    it "returns http success" do
+    it "returns http not_found code" do
       get :not_found
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:missing)
+    end
+  end
+
+  describe "GET #server_error" do
+    it "returns http server error" do
+      get :server_error
+      expect(response).to have_http_status(:error)
     end
   end
 
