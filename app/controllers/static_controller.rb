c8becd8b0e78e77642da18f90c7e269dd383f349
keita mori <keitamori@gmail.com>
2016-01-07 22:13:25 +0100

    Simple swagger controller documentation

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,17 @@
 class StaticController < ApplicationController
+  swagger_controller :static, "Static pages"
+
+  swagger_api :index do
+    summary "Show the main page"
+    response :ok, "Success"
+  end
+
   def index
+  end
+
+  swagger_api :about do
+    summary "Show the about page"
+    response :ok, "Success"
   end
 
   def about
